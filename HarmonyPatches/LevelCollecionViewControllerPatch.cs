fc3bb9286ee75aa1bd74e5b42acaf0d9dbee32f0
denpadokei <jsoldenpa@yahoo.co.jp>
2024-07-28 00:36:17 +0900

    change sortingOrder patch

---
+++ HarmonyPatches/LevelCollecionViewControllerPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace EnhancedStreamChat.HarmonyPatches
+{
+    [HarmonyPatch(typeof(LevelCollectionViewController), "DidActivate")]
+    internal class LevelCollecionViewControllerPatch
+    {
+        public static void Prefix(LevelCollectionViewController __instance, bool firstActivation)
+        {
+            if (firstActivation) {
+                var canvas = __instance.GetComponent<Canvas>();
+                canvas.overrideSorting = true;
+                canvas.sortingOrder = 3;
+            }
+        }
+    }
+}
